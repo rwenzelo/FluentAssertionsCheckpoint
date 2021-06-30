d9580a6397377fe1ad8f4ba8d212e5ece10bb5c4
Wenzelow <32124@nelnet.net>
2021-06-30 10:35:42 -0500

    first add

---
+++ FluentAssertions/Class1.vb
@@ -0,0 +1,9 @@
+public class exampleClass {
+    public void additionMethod() {
+        var first = 3;
+        var second = 7;
+        var result = first + second;
+
+        result.Should().Be(10);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+
+namespace FluentAssertionsCheckpoint
+{
+    class Program
+    {
+            public class exampleClass {
+            public void additionMethod() {
+                var first = 3;
+                var second = 7;
+                var result = first + second;
+
+                result.Should().Be(10);
+            }
+            }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
